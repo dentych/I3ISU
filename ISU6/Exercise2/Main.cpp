7a8e15069a80b06a99fd50c37851a5f18ed6a46c
dentych <dennis@tychsen.me>
2015-03-24 12:46:42 +0100

    Working version of ex2

--- ISU6/Exercise2/Main.cpp
+++ ISU6/Exercise2/Main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <unistd.h>
+#include <stdlib.h>
 #include "../Exercise1/Message.h"
 #include "../Exercise1/MsgQueue.h"
 
@@ -12,26 +14,69 @@ struct Point3D : public Message {
 	int x,y,z;
 };
 
-int main() {
-	return 0;
+void printOutPoint(Point3D * point) {
+	cout << "Point3D print:" << endl;
+	cout << "X: " << point->x << ". Y: " << point->y << ". Z: " << point->z << "." << endl;
+}
+
+void printOutPointHandler(Message* msg, unsigned long id) {
+	switch (id) {
+		case DATA_REQ:
+		{
+			Point3D * point = static_cast<Point3D *>(msg);
+			printOutPoint(point);
+			break;
+		}
+		case DATA_CFM:
+			//Nothing.
+			break;
+		default:
+			cout << "Something went wrong. You shouldn't be here..?!" << endl;
+			break;
+	}
 }
 
 void * senderThread(void * arg) {
 	// Cast the void * arg to a MsgQueue.
 	MsgQueue* mq = static_cast<MsgQueue*> (arg);
-	
+
 	// Run as long as the program is running.
 	while(1) {
 		// Create new Point3D message (inherits from message)
 		Point3D * req = new Point3D;
-		req->x = 1;
-		req->y = 2;
-		req->z = 3;
-		
+		req->x = rand() % 100;
+		req->y = rand() % 100;
+		req->z = rand() % 100;
+
 		// Send the message to the MsgQueue with id = DATA_REQ (a request).
 		mq->send(DATA_REQ, req);
+
+		sleep(1);
 	}
 }
 
 void * receiverThread(void * arg) {
+	MsgQueue* mq = static_cast<MsgQueue*>(arg);
+
+	while(1) {
+		unsigned long id;
+		Message * msg = mq->receive(id);
+		printOutPointHandler(msg, id);
+		delete msg;
+	}
+}
+
+int main() {
+	// Create message queue
+	MsgQueue mq(10);
+	// Thread variables
+	pthread_t sender, receiver;
+	// Init threads
+	pthread_create(&sender, NULL, senderThread, &mq);
+	pthread_create(&receiver, NULL, receiverThread, &mq);
+	
+	pthread_join(sender, NULL);
+	pthread_join(receiver, NULL);
+
+	return 0;
 }
